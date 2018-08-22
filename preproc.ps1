aa5fcc88507fb4826291a0967f6c61ab312e4b6f
Roman E <livcey@gmail.com>
2018-08-22 17:43:20 +0300

    Converting seconds to milliseconds as DJin import requires

--- preproc.ps1
+++ preproc.ps1
@@ -1,7 +1,8 @@
 <#
 Preprocessor for Digispot II "Import from text file" module
 v1.00 2018-08-18 Initial release
-v1.01 2018-08-20 Some fixes. Requre PowerShell >2.0
+v1.01 2018-08-20 Some fixes. Requres PowerShell >2.0
+v1.02 2018-08-22 Converting seconds to milliseconds as DJin import requires
 
 Command line in import module settings:
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoProfile -ExecutionPolicy bypass -command "\\your-root-server\ROOT\IMP_FORMATS\preproc.ps1"
@@ -32,8 +33,8 @@ $utfFile = $inputFile + ".utf8.txt"
 
 # HEADERS - Set columns headers here, we'll use it later for skip empty and select
 # WIDTHS  - Set columns width here
-$headers = 'Time','s1','s2','IDnumber','s','s4','s5','Dur','s6','Dur2','s8','s9','s10','s11','s12','s13','Date','Name'
-$widths = @(5,     3,   1,   4,          8,   5,   4,   3,    1,   3,     5,   1,   2,    5,    1,    12,   8,     30)
+$headers = 'Time','s1','s2','IDnumber','s3','s4','s5','Duration','s6','Dur2','s8','s9','s10','s11','s12','s13','Date','Name'
+$widths = @(5,     3,   1,   4,         8,   5,   4,   3,         1,   3,     5,   1,   2,    5,    1,    12,   8,     30)
 <#
 07:45.00N1330        77594    030 030NNFSP EP     0            20180817P_08_02/2018_#                
 #>
@@ -50,20 +51,23 @@ Get-Content $utfFile -Encoding UTF8 | ForEach-Object {
         $pos += $width
     }
     [string]::Join(";",$columns)                                     # Joining array to string using ; as separator
-} | Set-Content $csvFile -Encoding UTF8                            # Saving to temp file because
+} | Set-Content $csvFile -Encoding UTF8                              # Saving to temp file because
                                                                      # we need to reopen it again as CSV with headers
 $array = Import-Csv $csvFile -Delim ';' -Header $headers -Encoding UTF8
 
 # SKIP - Set condition for skip
-$array = $array | Where-Object { $_.IDnumber -notlike "" }          # Skiping lines with empty IDNo
+$array = $array | Where-Object { $_.IDnumber -notlike "" }           # Skiping lines with empty IDNo
 
 # SELECT - Select fields for output
-$array | Select-Object Time,IDnumber,Dur,Date,Name |                     # Selecting meaningful fields
-    Export-Csv -Path $csvFile -NoType -Delim ';' -Encoding UTF8           # Saving back to CSV as UTF8 -NoType
+$array = $array | Select-Object Time,IDnumber,Duration,Date,Name              # Selecting meaningful fields
+$array | ForEach-Object {
+        [string]$_.Duration = [int]$_.Duration * 1000                # Making milliseconds from seconds - DJin import requires ms
+    } 
+$array | Export-Csv -Path $csvFile -NoType -Delim ';' -Encoding UTF8 # Saving back to CSV as UTF8 -NoType
                                                                      # What else? Now we need little more PS magic
 $array = Get-Content $csvFile -Encoding UTF8 | Select-Object -Skip 1 # 1. Removing first line with headers
 $array.Replace('";"',";").TrimStart('"').TrimEnd('"') |              # 2. Stripping "" in CSV:
     Out-File $outputFile -Force -Confirm:$false -Encoding "UTF8"     # Saving output file
 
-Remove-Item -Path $csvFile -Force                                          # and removing temp file
+Remove-Item -Path $csvFile -Force                                    # Removing temp files
 Remove-Item -Path $utfFile -Force
